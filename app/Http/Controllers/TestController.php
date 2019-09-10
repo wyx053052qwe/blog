4fc9c5bcf201ed05603479b6995353fb570e1095
wangyunxing <wangyunxing@localhost.localdomain>
2019-09-10 02:34:34 +0000

    添加数据

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class TestController extends Controller
+{
+    public function insert10k()
+    {
+        $mobile_arr=[
+            1 => 132,
+            2 => 138,
+            3 => 136,
+            4 => 150,
+            5 => 152,
+            6 => 153,
+            7 => 158,
+        ];
+        $emial_arr = [
+            1 => '@qq.com',
+            2 => '@139.com',
+            3 => '@baidu.com',
+            4 => '@xinlang.com'
+        ];
+        for ($i=0;$i<10000;$i++){
+            $data = [
+                'user_name' => Str::random(10),
+                'email' => Str::random(12) . $emial_arr[mt_rand(1,4)],
+                'mobile' => $mobile_arr[mt_rand(1,7)] . Str::random(8),
+                'age' => mt_rand(1,99)
+            ];
+            $id=UserModel::insertGetId($data);
+            dump($id);
+        }
+    }
+}
